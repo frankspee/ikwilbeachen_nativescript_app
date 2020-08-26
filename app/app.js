095e484136abfbec4007a5c71a61cf0bc710e3d9
Frank Spee <4194493+frankspee@users.noreply.github.com>
2020-08-26 14:59:21 +0200

    Added devtools config

--- app/app.js
+++ app/app.js
@@ -1,6 +1,10 @@
+import VueDevtools from 'nativescript-vue-devtools';
 import Vue from "nativescript-vue";
 
 import Home from "./components/Home";
+
+Vue.use(VueDevtools);
+// Vue.use(VueDevtools, { host: '192.168.1.42' }) // ONLY USE IT LIKE THIS WITH REAL DEVICE > IP PC
 
 new Vue({
     render: h => h('frame', [h(Home)]),
