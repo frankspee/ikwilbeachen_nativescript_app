c1179166177ebe7d273371070ffe1e27093e8819
Frank Spee <4194493+frankspee@users.noreply.github.com>
2020-08-26 14:26:29 +0200

    Add/remove a reservation with date string

--- app/components/Home.vue
+++ app/components/Home.vue
@@ -7,17 +7,19 @@
     <GridLayout rows="*, auto">
       <Label v-if="reservations.length == 0" row="0" class="info">
         <FormattedString>
-          <Span class="fas" text.decode="&#xf45f; " />
+          <Span class="fas" text.decode="&#xf45f;" />
           <Span :text="message" />
         </FormattedString>
       </Label>
-      <ListView v-if="reservations.length > 0" for="reservation in reservations" row="0">
-        <Label :text="reservation.date"></Label>
+      <ListView v-else for="reservation in reservations" row="0">
+        <v-template>
+          <Label :text="reservation.date" v-on:tap="onItemTap(reservation)"></Label>
+        </v-template>
       </ListView>
-      <Button row="1" class="-primary2" v-on:tap="onTap()">
+      <Button row="1" class="-primary" v-on:tap="onNewTap()">
         <FormattedString>
-          <Span class="fas" text="&#xf45f;"></Span>
-          <Span text=" Nieuw 2"></Span>
+          <Span class="far" text.decode="&#xf271;"></Span>
+          <Span text=" Nieuwe reservering"></Span>
         </FormattedString>
       </Button>
     </GridLayout>
@@ -33,12 +35,16 @@ export default {
   },
   computed: {
     message() {
-      return "Voeg een reservering toe";
+      return " Voeg een reservering toe";
     },
   },
   methods: {
-    onTap() {
-      // this.reservations.unshift({date:"1 augustus 2020"});
+    onNewTap() {
+      this.reservations.unshift({ date: "1 augustus 2020" });
+    },
+    onItemTap(reservation) {
+      const index = this.reservations.indexOf(reservation);
+      this.reservations.splice(index, 1);
     },
   },
 };
