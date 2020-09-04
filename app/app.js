8270c433eb66cd6c712dd13d553bb37bb95e6afb
Frank Spee <4194493+frankspee@users.noreply.github.com>
2020-09-04 13:28:52 +0200

    Initial addition of Vuex state

--- app/app.js
+++ app/app.js
@@ -1,11 +1,17 @@
-import VueDevtools from 'nativescript-vue-devtools';
 import Vue from "nativescript-vue";
 
 import Home from "./components/Home";
+import VueDevtools from 'nativescript-vue-devtools';
 
-Vue.use(VueDevtools);
-// Vue.use(VueDevtools, { host: '192.168.1.42' }) // ONLY USE IT LIKE THIS WITH REAL DEVICE > IP PC
+import store from './store';
+
+if (TNS_ENV !== 'production') {
+    Vue.use(VueDevtools);
+    // Vue.use(VueDevtools, { host: '192.168.1.42' }) // ONLY USE IT LIKE THIS WITH REAL DEVICE > IP PC
+}
+// Prints Vue logs when --env.production is *NOT* set while building
+Vue.config.silent = TNS_ENV === 'production';
 
 new Vue({
     render: h => h('frame', [h(Home)]),
-}).$start();
+}, store).$start();
---
+++ app/store/index.js
@@ -0,0 +1,21 @@
+import Vue from 'nativescript-vue';
+import Vuex from 'vuex';
+
+Vue.use(Vuex);
+
+const debug = process.env.NODE_ENV !== 'production';
+
+const store = new Vuex.Store({
+    state: {
+      count: 0
+    },
+    mutations: {
+      increment: state => state.count++,
+      decrement: state => state.count--
+    },
+    strict: debug
+})
+
+Vue.prototype.$store = store;
+
+export default store;
